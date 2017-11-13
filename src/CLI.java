260f72aef46d2627e26a0d32113a2801280ac622
Derek Lopes <derkle@live.com>
2017-11-12 23:52:03 -0800

    Added ability to create a student account

--- src/CLI.java
+++ src/CLI.java
@@ -1,36 +1,64 @@
-import java.sql.ResultSet;
 import java.util.Scanner;
 
 public class CLI {
     public static void main(String[] args) {
         boolean exit = false;
-        while (!exit) {
+        boolean loggedIn = false;
+        String loginType = "none";
+        Integer ID = -1;
+        // Log in screen
+        while (!exit && !loggedIn) {
             System.out.println("\n~~~ CollegeHero v0.1 - CS157A Fall 2017 ~~~\n");
-            System.out.println("What would you like to do:");
+            System.out.println("Please sign in:");
             System.out.println(
                     "[0] Exit\n" +
-                    "[1] Get all student information\n" +
-                    "[2] Get all staff information"
+                            "[1] Login as student\n" +
+                            "[2] Login as teacher\n" +
+                            "[3] Register as new student\n" +
+                            "[4] Search for ID by phone nubmer"
             );
-            Integer[] options = {0, 1, 2};
+            Integer[] options = {0, 1, 2, 3, 4};
             Integer decision = getOption(options);
             switch (decision) {
                 case 0:
                     System.out.println("Exiting...");
                     exit = true;
                     break;
-                case 1:
-                    getStudents();
+                case 3:
+                    ID = createStudent();
+                    if (ID > 0) {
+                        loggedIn = true;
+                        loginType = "s";
+                    }
                     break;
-                case 2:
-                    getStaff();
-                    break;
+                default:
+                    System.out.println("This feature is not implemented yet");
             }
+        }
+
+        // Studnet action screen
+        if (loginType.equals("s")) {
+            clearConsole();
+            System.out.printf("--- Logged in as student ID: %s ---\n", ID.toString());
+            System.out.println("Action phase, later you can do things like enroll in a class");
+        }
+        // Teacher action screen
+        if (loginType.equals("t")) {
+            clearConsole();
+            System.out.printf("--- Logged in as student ID: %s ---\n", ID.toString());
+            System.out.println("Action phase, later you can do things like take attendance");
+        }
+        // Admin action screen
+        if (loginType.equals("a")) {
+            clearConsole();
+            System.out.printf("--- Logged in as student ID: %s ---\n", ID.toString());
+            System.out.println("Action phase, later you can do things like create a class");
         }
     }
 
     /**
      * Prompts for input and gets a valid response
+     *
      * @param options - integer array to validate input against
      * @return user's choice, validated
      */
@@ -67,5 +95,33 @@ public class CLI {
      */
     private static void getStaff() {
         DatabaseConnector.printAllFromTable("staff");
+    }
+
+    private static int createStudent() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Enter your full name: ");
+        String name = scanner.nextLine();
+        // TODO: Change password retrieval to not show characters inputted
+        System.out.printf("Enter your password: ");
+        String password = scanner.nextLine();
+        System.out.printf("Enter your sex (1 for male, 0 for female): ");
+        int sexInput = getOption(new Integer[]{0, 1});
+        boolean sex = true;
+        if (sexInput == 0)
+            sex = false;
+        System.out.printf("Enter your phone number (ex. 5555555555): ");
+        String phone = scanner.nextLine();
+        Integer sID = DatabaseConnector.createStudent(name, password, sex, phone);
+        if (sID > 0) {
+            System.out.println("Success. Your student ID is: " + sID.toString());
+            return sID;
+        }
+        System.out.println("There was an error creating your account.");
+        return -1;
+    }
+
+    private static void clearConsole() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
